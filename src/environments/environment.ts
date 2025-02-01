fc6e32d4194a990ae62747c71b769726198b30d2
LilGhostly <144309115+LilGhostly@users.noreply.github.com>
2025-02-01 12:56:56 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCaTUF0-VCPwwvE7vK9-C1BwH-kFHPeXLg",
-    authDomain: "mystore-c2cd3.firebaseapp.com",
-    databaseURL: "https://mystore-c2cd3-default-rtdb.firebaseio.com/",
-    projectId: "mystore-c2cd3",
-    storageBucket: "mystore-c2cd3.firebasestorage.app",
-    messagingSenderId: "409515667628",
-    appId: "1:409515667628:web:7f00968c934f91c52baf09"
+
   }
 };
 
